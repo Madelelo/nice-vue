93e245383cd868caddf5f11885f3c49079c8468f
Kshitij Aranke <kshitij.aranke@gmail.com>
2016-11-15 07:08:09 -0600

    finish vue

--- app.js
+++ app.js
@@ -1,39 +1,79 @@
 var apiURL = 'https://frozen-temple-72096.herokuapp.com/api/movies'
 
-/**
- * Actual demo
- */
-
-var demo = new Vue({
-
-  el: '#demo',
-
-  data: {
-    commits: null
+// register the grid component
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  replace: true,
+  props: {
+    data: Array,
+    columns: Array,
+    filterKey: String
   },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  computed: {
+    filteredData: function () {
+      var sortKey = this.sortKey
+      var filterKey = this.filterKey && this.filterKey.toLowerCase()
+      var order = this.sortOrders[sortKey] || 1
+      var data = this.data
+      if (filterKey) {
+        data = data.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+          })
+        })
+      }
+      if (sortKey) {
+        data = data.slice().sort(function (a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return data
+    }
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
 
+// bootstrap the demo
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: ['title', 'releaseYear', 'avgRating'],
+    gridData: []
+  },
   created: function () {
     this.fetchData()
   },
-
-  filters: {
-    truncate: function (v) {
-      var newline = v.indexOf('\n')
-      return newline > 0 ? v.slice(0, newline) : v
-    },
-    formatDate: function (v) {
-      return v.replace(/T|Z/g, ' ')
-    }
-  },
-
   methods: {
     fetchData: function () {
       var xhr = new XMLHttpRequest()
       var self = this
       xhr.open('GET', apiURL, true)
       xhr.onload = function () {
-        var commits = JSON.parse(xhr.responseText)
-        self.commits = commits.movieList;
+        var movies = JSON.parse(xhr.responseText)
+        self.gridData = movies.movieList;
       }
       xhr.send()
     }
